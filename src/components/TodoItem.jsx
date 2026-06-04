76e7655be5091fa2d2ffa3ba260c0c2c87e995f5
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-06-03 23:56:58 -0700

    feat: update the set agenda state to update each addition on lo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import styles from "./todoitem.module.css";
 
 const TodoItem = ({ item }) => {
+  const handleDelete = (item) => {
+    console.log("delete button clicked for item", item);
+  };
   return (
     <div className={styles.item}>
       <div key={item} className={styles.itemname}>
         {item}{" "}
         <span>
-          <button>X</button>
+          <button
+            className={styles.deletebutton}
+            onClick={() => handleDelete(item)}
+          >
+            x
+          </button>
         </span>
       </div>
 
